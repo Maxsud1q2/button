856ec70b522c24c449010377cb4ca2c65c0e99ab
maxsud210 <kparking161@gmail.com>
2024-04-24 19:21:13 +0500

    comment

---
+++ script.js
@@ -0,0 +1,8 @@
+const hh = document.querySelector('.hh');
+const aa = document.querySelector('.aa');
+const bb = document.querySelector('.bb');
+const ff = document.querySelector('.ff');
+
+bb.addEventListener('click', function() {
+    
+})
